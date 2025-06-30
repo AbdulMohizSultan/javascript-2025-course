f440c0c30aa221af5e6d7bcc078875aa10046b1d
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-06-30 18:55:05 +0500

    Data Types

---
+++ new course/script.js
@@ -0,0 +1,70 @@
+// //                                                                              var let const
+// //                                                                              declarations and initialization
+
+//  let a = 10;
+//  let b = 23;
+
+//  const   a =100;
+
+// //                                                                             remporal Dead Zone
+
+// console.log(a)
+
+
+
+// let a =12;
+
+// //                                                                              hoisting
+
+// // var -> hoist -> undefined
+// // let -> hoist -> X
+// // const -> hoist -> X
+
+
+// //                                                                             practice
+
+// console.log(mn)
+// var mn = "mohiz"
+
+// console.log(age)
+// let age = 25 
+
+// //                                                                             gobal
+
+// var a = 10;
+
+// {
+//      var a = 20
+//     console.log("inside:", a);
+// }
+
+// console.log("ontside", a )
+
+////                                                                              data type
+
+////                                                                 1)primities  (string. number, boolean, null, undefined, symbol, bigint)
+////                                                                 2)reference  (arrays, object, function)
+
+
+////                                                                           dynamic typing
+
+let a = 12;
+a = true;
+a = "harsh" ;
+a = [];
+a = null;
+a = undefined;
+
+////                                                                           type of quirks 
+
+////  problem in javascript 
+
+////                                                                          type coercion (== vs === )
+////                                                                          truthy vs falsy values
+
+////                                                                           false value
+////   !!""
+////   !!NaN
+////   !!null
+////   !!undefined
+////
